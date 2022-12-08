3f45b433e5af85e84583c3b2760ef2b1f6a23d45
Alexander Hill <ahill@breadpudding.dev>
2022-12-07 20:54:27 -0500

    Repaired my communication system

---
+++ day06/src/part2.rs
@@ -0,0 +1,29 @@
+use std::{collections::VecDeque, fs};
+
+fn is_unique(list: &VecDeque<char>) -> bool {
+    for x in 0..(list.len() - 1) {
+        for y in (x + 1)..list.len() {
+            if list[x] == list[y] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn main() {
+    let mut buffer = VecDeque::new();
+    // This probably isn't an optimization problem... right?
+    let input = fs::read_to_string("data.txt").unwrap();
+    for (i, c) in input.char_indices() {
+        buffer.push_back(c);
+        // This is the only change in the part 2 file...
+        if buffer.len() > 13 {
+            if is_unique(&buffer) {
+                println!("Start of packet marker detected at character {}", i + 1);
+                break;
+            }
+            buffer.pop_front();
+        }
+    }
+}
